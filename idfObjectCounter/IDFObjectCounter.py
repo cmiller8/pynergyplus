457a0dc56d13c009965ddc434e6b28bc6c45f977
cmiller8 <miller.clayton@gmail.com>
2011-05-19 16:51:36 +0800

    IDFObjectCounter - Added Counter and Print Function

--- idfObjectCounter/IDFObjectCounter.py
+++ idfObjectCounter/IDFObjectCounter.py
@@ -13,26 +13,29 @@ def main():
   
   # Define input path for IDF File and Output path for report
   fInIDF = open('in.idf','r')
-#  fInIDD = open('Energy+.idd','r')
-
-#calls readlines which puts every line of the IDD into a list
-#  Lines = fInIDD.readlines()
-#  for line in Lines:
-#    print line,
   
+  # Create and IDF Object List and add all lines where the first char is NOT a ' '
   IDFObjectList = []
-
   Lines = fInIDF.readlines()
   for line in Lines:
     firstchar = line[0]
     if not firstchar.isspace() and not firstchar == '!':
+      line = line.replace(',','')
       IDFObjectList.append(line)
-  
+      
+  # Sort the List and create a dictionary with the number of occurences
   IDFObjectList.sort()
-  IDFObjects = {}
   
-
-
+  IDFObjects = {}
+  for object in IDFObjectList:
+    if not object in IDFObjects:
+      IDFObjects[object] = 1
+    else:
+      IDFObjects[object] = IDFObjects[object] + 1
+      
+  # Prints the dictionary when in.idf is in the same folder 
+  for object in IDFObjects:
+    print object + str(IDFObjects[object])
 
 if __name__ == '__main__':
   main()
